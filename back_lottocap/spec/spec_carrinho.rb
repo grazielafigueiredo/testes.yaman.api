88e2d9d8ab0cd9323fbf15cd56bf7f41cf0b461d
Graziela Figueiredo <grazielafigueiredo@air-de-graziela.lan>
2019-12-05 10:37:44 -0300

    add item de verificacao

--- back_lottocap/spec/spec_carrinho.rb
+++ back_lottocap/spec/spec_carrinho.rb
@@ -35,9 +35,9 @@ describe 'Carrinho - Reserva' do
 
       @result = ApiCarrinho.get_GetStatusCarrinho
     end
-    it { expect(@result.response.code).to eql '200' }
     it { expect(JSON.parse(@result.response.body)['dadosUsuario']['carrinhoItens'][0]['dtFimConcurso']).to eql '0001-01-01T00:00:00' }
-  
+    it { puts @result.response.body}
+
     after do 
       ApiCarrinho.post_SetRemoverItemCarrinho(@token, @idCarrinho)
       ApiUser.get_deslogar(@token)
@@ -75,8 +75,8 @@ describe 'Carrinho - Reserva' do
       @carrinho = ApiCarrinho.get_GetStatusCarrinho
     end
 
-    it { expect(@result.response.code).to eql '200' }
     it { expect(JSON.parse(@carrinho.response.body)['erros'][0]['mensagem']).to eql 'Atualizamos o carrinho mantendo apenas as série em andamento' }
+    it { puts @result.response.body}
 
     after do
       Database.new.update_DataFinalVendaVigente
@@ -95,7 +95,7 @@ describe 'Carrinho - Reserva' do
       @carrinho = ApiCarrinho.get_GetStatusCarrinho
     end
 
-    it { expect(@result.response.code).to eql '200' }
+    it { puts @result.response.body}
     it { expect(JSON.parse(@carrinho.response.body)['erros'][0]['mensagem']).to eql 'A quantidade de títulos que você deseja para o LottoCap Max - Max Série Nova (id 86) não está mais disponível. Atualizamos seu carrinho com a quantidade disponível!' }
    
     after do
@@ -118,6 +118,7 @@ describe 'Carrinho - Reserva' do
       @carrinho = ApiCarrinho.get_GetStatusCarrinho
     end
 
+    it { puts @result.response.body}
     it { expect(JSON.parse(@carrinho.response.body)['erros'][0]['mensagem']).to eql 'Atualizamos o carrinho mantendo apenas as série em andamento' }
 
     after do
@@ -171,7 +172,7 @@ describe 'Carrinho - Sem Reserva - Tentar Pagar' do
       @carrinho = ApiCartao.post_AdicionarCartaoDeCreditoSucesso(@token, @idCarrinho)
     end
 
-    it { expect(@result.response.code).to eql '200' }
+    it { puts @result.response.body}
     it { expect(JSON.parse(@carrinho.response.body)['erros'][0]['mensagem']).to eql "Esta forma de pagamento não está mais disponível, por favor. Selecione outra forma de pagamento."}
 
     after do
@@ -191,9 +192,10 @@ describe 'Carrinho - Sem Reserva - Tentar Pagar' do
 
       Database.new.update_AtualizarSerieAndamento
 
-      @carrinho = ApiTransferencia.post_TransfSucessoBradesco(@token, @idCarrinho)
+      @result = ApiTransferencia.post_TransfBradesco(@token, @idCarrinho, Faker::Bank.account_number(digits: 4), Faker::Bank.account_number(digits: 4), Faker::Bank.account_number(digits: 1), Constant::NomeCompletoTitular)
     end
 
+    it { puts @result.response.body}
     it { expect(JSON.parse(@carrinho.response.body)['erros'][0]['mensagem']).to eql "Esta forma de pagamento não está mais disponível, por favor. Selecione outra forma de pagamento."}
 
     after do
@@ -216,6 +218,7 @@ describe 'Carrinho - Sem Reserva - Tentar Pagar' do
       @carrinho = ApiBoleto.post_SucessoBoleto(@token, @idCarrinho)
     end
 
+    it { puts @result.response.body}
     it { expect(JSON.parse(@carrinho.response.body)['erros'][0]['mensagem']).to eql "Esta forma de pagamento não está mais disponível, por favor. Selecione outra forma de pagamento."}
 
     after do
@@ -230,21 +233,22 @@ describe 'Carrinho - Sem Reserva - Tentar Pagar' do
       @token = ApiUser.GetToken
       ApiUser.Login(@token, Constant::User1)
 
-      Database.new.update_insertCreditoLottocap
+      Database.new.update_CreditoLottocap(100.000)
 
       @result = ApiCarrinho.post_AdicionarItemCarrinho(1, Constant::IdProduto, Constant::IdSerie, @token)
       @idCarrinho = JSON.parse(@result.response.body)['obj'][0]['idCarrinho']
 
       Database.new.update_AtualizarSerieAndamento
 
-      @carrinho = ApiCreditoLottocap.post_SucessoCreditoLottocap(@token, @idCarrinho)
+      @carrinho = ApiCreditoLottocap.post_CreditoLottocap(@token, @idCarrinho)
     end
 
+    it { puts @result.response.body}
     it { expect(JSON.parse(@carrinho.response.body)['erros'][0]['mensagem']).to eql "Esta forma de pagamento não está mais disponível, por favor. Selecione outra forma de pagamento."}
   
     after do
       Database.new.update_DataFinalVendaVigente
-      Database.new.update_deleteCreditoLottocap
+      Database.new.update_CreditoLottocap(0.000)
   
       ApiCarrinho.post_SetRemoverItemCarrinho(@token, @idCarrinho)
       ApiUser.get_deslogar(@token)
@@ -266,6 +270,7 @@ describe 'Carrinho - Sem Reserva - Tentar Pagar' do
       
     end
     
+    it { puts @result.response.body}
     it { expect(JSON.parse(@carrinho.response.body)['erros'][0]['mensagem']).to eql "Esta forma de pagamento não está mais disponível, por favor. Selecione outra forma de pagamento."}
 
     after do
