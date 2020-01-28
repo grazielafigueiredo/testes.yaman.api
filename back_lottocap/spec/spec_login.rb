c1433f2fbea2389f1c80db18ebae5cb62f7d18e9
Graziela Figueiredo <grazielafigueiredo@air-de-graziela.lan>
2020-01-28 15:32:30 -0300

    login refatorado

--- back_lottocap/spec/spec_login.rb
+++ back_lottocap/spec/spec_login.rb
@@ -1,26 +1,25 @@
+# frozen_string_literal: true
+
 context 'Erro 400 API Logar Usuário - só resolveu limpando service worker' do
   before do
     @token = ApiUser.GetToken
     ApiUser.Login(@token, Constant::User1)
 
     def execute_script
-        
       js_script = 'window.localStorage.setItem("tokenAutenticado", "false");'
-          token = page.execute_script(js_script)
-              expect(token).to eql "false"
-              puts token
+      token = page.execute_script(js_script)
+      expect(token).to eql 'false'
+      puts js_script
     end
-      
 
     @login = ApiUser.Login(@token, Constant::User1)
-      expect(JSON.parse(@login.response.code)).to be 401
+    expect(JSON.parse(@login.response.code)).to be 401
 
-  
     @token = ApiUser.GetToken
     @loginNovamente = ApiUser.Login(@token, Constant::User1)
   end
 
-  it 'validacao de login' do 
+  it 'validacao de login' do
     expect(JSON.parse(@loginNovamente.response.body)['obj'][0]).to be true
     expect(JSON.parse(@loginNovamente.response.body)['sucesso']).to eql true
     expect(JSON.parse(@loginNovamente.response.code)).to be 200
