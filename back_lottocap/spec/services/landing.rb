76ae224998764bab4716e2453167a996019ffd65
Graziela Figueiredo <grazielafigueiredo@air-de-graziela.lan>
2020-02-06 12:39:52 -0300

    teste de landing Já

--- back_lottocap/spec/services/landing.rb
+++ back_lottocap/spec/services/landing.rb
@@ -7,9 +7,16 @@ class ApiLanding
   base_uri Constant::Url
   headers 'Content-Type' => 'application/json'
 
-  def self.get_LandingPageMax()
+  def self.get_landingPageMax()
 
 
-    get('/Produto/LandingPageMax', body: @LandingPageMax.to_json)
+    get('/Produto/LandingPageMax', body: @landingPageMax.to_json)
   end
+
+  def self.get_landingPageJa()
+
+
+    get('/Produto/LandingPageJa', body: @landingPageJa.to_json)
+  end
+
 end
--- back_lottocap/spec/spec_landing.rb
+++ back_lottocap/spec/spec_landing.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
 
-describe 'Landing' do
+describe 'Landing MAX' do
   context 'Sucesso' do
     before do
 
-      @result = ApiLanding.get_LandingPageMax()
+      @result = ApiLanding.get_landingPageMax()
+      puts @result
+    end
+    it { expect(JSON.parse(@result.response.body)['sucesso']).to be true }
+  end
+end
+
+describe 'Landing JÁ' do
+  context 'Sucesso' do
+    before do
+
+      @result = ApiLanding.get_landingPageJa()
       puts @result
     end
     it { expect(JSON.parse(@result.response.body)['sucesso']).to be true }
