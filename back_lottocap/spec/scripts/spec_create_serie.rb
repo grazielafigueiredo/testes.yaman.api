f460a4bdc74f829f8c8b617589d63713041446aa
Graziela Figueiredo <grazielafigueiredo@MacBook-Air-de-Graziela.local>
2020-06-09 20:12:15 -0300

    indentacao

--- back_lottocap/spec/scripts/spec_create_serie.rb
+++ back_lottocap/spec/scripts/spec_create_serie.rb
@@ -6,20 +6,21 @@
 #     # templateSerieId = 4 #18
 #     # TituloMatrizDEV.new.relacionamento_serie_concurso_ja(mes, templateSerieId)
 # end
+describe 'serie' do
+  context '[MAX] - Criação de série/concurso' do
+    mes = 9 # parametro mes da série/concurso
+    TituloMatrizDB.new.relacionamento_serie_concurso(mes)
+  end
 
-context '[MAX] - Criação de série/concurso' do
-  mes = 9 # parametro mes da série/concurso
-  TituloMatrizDB.new.relacionamento_serie_concurso(mes)
-end
+  context '[JA 17] - Criação de série/concurso Ja' do
+    mes = 9
+    idProduto_ja = 9
+    TituloMatrizDB.new.relacionamento_serie_concurso_ja(mes, idProduto_ja)
+  end
 
-context '[JA 17] - Criação de série/concurso Ja' do
-  mes = 9
-  idProduto_ja = 9
-  TituloMatrizDB.new.relacionamento_serie_concurso_ja(mes, idProduto_ja)
-end
-
-context '[JA 18] - Criação de série/concurso Ja' do
-  mes = 9
-  idProduto_ja = 10
-  TituloMatrizDB.new.relacionamento_serie_concurso_ja(mes, idProduto_ja)
+  context '[JA 18] - Criação de série/concurso Ja' do
+    mes = 9
+    idProduto_ja = 10
+    TituloMatrizDB.new.relacionamento_serie_concurso_ja(mes, idProduto_ja)
+  end
 end
--- back_lottocap/spec/spec_producao.rb
+++ back_lottocap/spec/spec_producao.rb
@@ -1,102 +1,100 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
 describe 'Criar usuário' do
-
-    context 'CPF já cadastrado' do
-        before do
-            @token = ApiProducao.GetToken
-            @create = ApiProducao.post_validarDadosUsuarioCriacao(@token, Faker::Name.name, "00000009652", Faker::Internet.email)
-        puts @create  
-        puts @token
-        end
-  
-        it { expect((@create.parsed_response)['erros'][0]['mensagem']).to eql "Este CPF já está cadastrado. Informe um outro CPF ou clique em 'Esqueci a minha senha'."}
-  
-        after do
-          ApiProducao.get_logout(@token)
-        end
+  context 'CPF já cadastrado' do
+    before do
+      @token = ApiProducao.GetToken
+      @create = ApiProducao.post_validarDadosUsuarioCriacao(@token, Faker::Name.name, '00000009652', Faker::Internet.email)
+      puts @create
+      puts @token
     end
 
-    context 'CPF inválido' do
-        before do
-          @token = ApiProducao.GetToken
-          @create = ApiProducao.post_CadastrarUsuario(@token, Faker::Name.name, "00000000000", Faker::Internet.email)
-        end
-  
-        it { expect((@create.parsed_response)['obj.CPF'][0]).to eql "O campo CPF contém dados inválidos."}
-  
-        after do
-          ApiProducao.get_logout(@token)
-        end
+    it { expect(@create.parsed_response['erros'][0]['mensagem']).to eql "Este CPF já está cadastrado. Informe um outro CPF ou clique em 'Esqueci a minha senha'." }
+
+    after do
+      ApiProducao.get_logout(@token)
+    end
+  end
+
+  context 'CPF inválido' do
+    before do
+      @token = ApiProducao.GetToken
+      @create = ApiProducao.post_CadastrarUsuario(@token, Faker::Name.name, '00000000000', Faker::Internet.email)
     end
 
-    context 'Email já cadastrado' do
-        before do
-          @token = ApiProducao.GetToken
-          @create = ApiProducao.post_CadastrarUsuario(@token, Faker::Name.name, Faker::CPF.numeric, "graziela@lottocap.com.br")
-        end
-  
-        it { expect((@create.parsed_response)['erros'][0]['mensagem']).to eql "Este e-mail já está cadastrado. Informe um outro e-mail ou clique em 'Esqueci a minha senha'."}
-  
-        after do
-          ApiProducao.get_logout(@token)
-        end
+    it { expect(@create.parsed_response['obj.CPF'][0]).to eql 'O campo CPF contém dados inválidos.' }
+
+    after do
+      ApiProducao.get_logout(@token)
     end
+  end
+
+  context 'Email já cadastrado' do
+    before do
+      @token = ApiProducao.GetToken
+      @create = ApiProducao.post_CadastrarUsuario(@token, Faker::Name.name, Faker::CPF.numeric, 'graziela@lottocap.com.br')
+    end
+
+    it { expect(@create.parsed_response['erros'][0]['mensagem']).to eql "Este e-mail já está cadastrado. Informe um outro e-mail ou clique em 'Esqueci a minha senha'." }
+
+    after do
+      ApiProducao.get_logout(@token)
+    end
+  end
 end
 
-describe 'Validador - 1 passo de criação de usuário' do 
-    context 'CPF já cadastrado' do
-        before do
-            @token = ApiProducao.GetToken
-            @create = ApiProducao.post_validarDadosUsuarioCriacao(@token, Faker::Name.name, "00000009652", Faker::Internet.email)
-        end
-  
-        it { expect((@create.parsed_response)['erros'][0]['mensagem']).to eql "Este CPF já está cadastrado. Informe um outro CPF ou clique em 'Esqueci a minha senha'."}
-  
-        after do
-          ApiProducao.get_logout(@token)
-        end
+describe 'Validador - 1 passo de criação de usuário' do
+  context 'CPF já cadastrado' do
+    before do
+      @token = ApiProducao.GetToken
+      @create = ApiProducao.post_validarDadosUsuarioCriacao(@token, Faker::Name.name, '00000009652', Faker::Internet.email)
     end
 
-    context 'CPF inválido' do
-        before do
-          @token = ApiProducao.GetToken
-          @create = ApiProducao.post_validarDadosUsuarioCriacao(@token, Faker::Name.name, "00000000000", Faker::Internet.email)
-        end
-  
-        it { expect((@create.parsed_response)['obj.CPF'][0]).to eql "O campo CPF contém dados inválidos."}
-  
-        after do
-          ApiProducao.get_logout(@token)
-        end
+    it { expect(@create.parsed_response['erros'][0]['mensagem']).to eql "Este CPF já está cadastrado. Informe um outro CPF ou clique em 'Esqueci a minha senha'." }
+
+    after do
+      ApiProducao.get_logout(@token)
+    end
+  end
+
+  context 'CPF inválido' do
+    before do
+      @token = ApiProducao.GetToken
+      @create = ApiProducao.post_validarDadosUsuarioCriacao(@token, Faker::Name.name, '00000000000', Faker::Internet.email)
     end
 
-    context 'Email já cadastrado' do
-        before do
-          @token = ApiProducao.GetToken
-          @create = ApiProducao.post_validarDadosUsuarioCriacao(@token, Faker::Name.name, Faker::CPF.numeric, "graziela@lottocap.com.br")
-        end
-  
-        it { expect((@create.parsed_response)['erros'][0]['mensagem']).to eql "Este e-mail já está cadastrado. Informe um outro e-mail ou clique em 'Esqueci a minha senha'."}
-  
-        after do
-          ApiProducao.get_logout(@token)
-        end
+    it { expect(@create.parsed_response['obj.CPF'][0]).to eql 'O campo CPF contém dados inválidos.' }
+
+    after do
+      ApiProducao.get_logout(@token)
     end
-end 
+  end
 
+  context 'Email já cadastrado' do
+    before do
+      @token = ApiProducao.GetToken
+      @create = ApiProducao.post_validarDadosUsuarioCriacao(@token, Faker::Name.name, Faker::CPF.numeric, 'graziela@lottocap.com.br')
+    end
 
-describe 'Alterar dados Usuário' do 
+    it { expect(@create.parsed_response['erros'][0]['mensagem']).to eql "Este e-mail já está cadastrado. Informe um outro e-mail ou clique em 'Esqueci a minha senha'." }
+
+    after do
+      ApiProducao.get_logout(@token)
+    end
+  end
+end
+
+describe 'Alterar dados Usuário' do
   context 'Alterar cpf para número inválido' do
     before do
       @token = ApiProducao.GetToken
       ApiProducao.Login(@token, Constant::USER)
 
-      @create = ApiProducao.post_alterarDadosUsuario(@token, "graziela@lottocap.com.br", "00000000000")
+      @create = ApiProducao.post_alterarDadosUsuario(@token, 'graziela@lottocap.com.br', '00000000000')
       puts @create
-    end 
+    end
 
-    it { expect((@create.parsed_response)['obj.cpf'][0]).to eql "O campo cpf contém dados inválidos."}
+    it { expect(@create.parsed_response['obj.cpf'][0]).to eql 'O campo cpf contém dados inválidos.' }
   end
   after do
     ApiProducao.get_logout(@token)
